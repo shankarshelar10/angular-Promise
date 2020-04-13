086dc9fe80d77cd85e229f0d2db4d7c09e26b535
shankarshelar10 <shankarshelar10@gmail.com>
2020-04-13 18:10:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-4n1flh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-4n1flh)
---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component , OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent implements OnInit {
+  name = 'Angular';
+  apiUrl = 'https://www.techiediaries.com/api/data.json';
+  constructor(private httpClient : HttpClient){
+
+  }
+
+  ngOnInit(){
+    this.fetchData();
+  }
+
+  private fetchData() {
+    const promise = this.httpClient.get(this.apiUrl).toPromise();
+    console.log(promise);
+
+    promise.then((data) => {
+            console.log("Promise rejected with " + JSON.stringify(data));
+    }, (error) => {
+            console.log("Promise rejected with " + JSON.stringify(error));
+
+    })
+  }
+}
